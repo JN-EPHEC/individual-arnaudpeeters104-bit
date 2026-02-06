2f32404f93f270c66285d488f4050e635460e5c5
Arnaud <arnaudpeeters104@gmail.com>
2026-02-06 01:49:22 +0100

    exo 5 parti 1

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res) => {
+    res.send('Bienvenue sur mon serveur API')
+});
+
+app.listen(port, () => {
+    console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
 function greet(name: string): string {
     return `Hello, ${name}!`;
 }
